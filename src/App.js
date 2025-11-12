2e841883476c6194b402f330e9d0bde6894058f6
vah-ram <vahram.s.ghazaryan@gmail.com>
2025-11-13 01:37:19 +0400

    changed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Register from './registration/register';
 import Login from './registration/login';
+import Project from './Project';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Project/>}/>
           <Route path='/register' element={<Register/>}/>
           <Route path='/login' element={<Login/>}/>
         </Routes>
---
+++ src/Project.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Project() {
+  return (
+    <div>
+        <a href="/login">Log in</a>
+        <a href="/register">Sign up</a>
+    </div>
+  )
+}
+
+export default Project
