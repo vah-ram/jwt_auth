52aa78adb65c21ee0b5e726c559bd1c016729284
vah-ram <vahram.s.ghazaryan@gmail.com>
2025-11-13 15:55:33 +0400

    changed backend host

--- src/Hosts.js
+++ src/Hosts.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000"
+export const host = "https://jwt-auth-backend-c5gm.onrender.com"
 
 export const registerHost = `${host}/api/register`
 export const loginHost = `${host}/api/login`
